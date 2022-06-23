57ed8ae26f781335762d7085dcbb10b52af02265
Tristan Matthews <tmatth@videolan.org>
2022-06-23 12:27:29 -0400

    streaming2ffmpeg: use galene's KeyFrame detection for h.264
    We still don't have a non-empty mkv, need to extract dimensions from SPS for a start.

--- janus-gateway/streaming2ffmpeg/main.go
+++ janus-gateway/streaming2ffmpeg/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os/exec"
+	"strings"
 	"time"
 
 	janus "github.com/notedit/janus-go"
@@ -95,7 +96,7 @@ func startFFmpeg(width, height int) {
 				Name:            "Video",
 				TrackNumber:     2,
 				TrackUID:        67890,
-				CodecID:         "V_VP8",
+				CodecID:         "V_MPEG4/ISO/AVC",
 				TrackType:       1,
 				DefaultDuration: 33333333,
 				Video: &webm.Video{
@@ -132,6 +133,231 @@ func pushOpus(rtpPacket *rtp.Packet) {
 	}
 }
 
+// These come verbatim from galene, Copyright (c) 2020 by Juliusz Chroboczek (MIT License)
+// source: https://github.com/jech/galene/blob/40bf93cdd04b71d66b94e68ccbff8265724485b9/codecs/codecs.go#L13
+
+// Keyframe determines if packet is the start of a keyframe.
+// It returns (true, true) if that is the case, (false, true) if that is
+// definitely not the case, and (false, false) if the information cannot
+// be determined.
+func Keyframe(codec string, packet *rtp.Packet) (bool, bool) {
+	if strings.EqualFold(codec, "video/vp8") {
+		var vp8 codecs.VP8Packet
+		_, err := vp8.Unmarshal(packet.Payload)
+		if err != nil || len(vp8.Payload) < 1 {
+			return false, false
+		}
+
+		if vp8.S != 0 && vp8.PID == 0 && (vp8.Payload[0]&0x1) == 0 {
+			return true, true
+		}
+		return false, true
+	} else if strings.EqualFold(codec, "video/vp9") {
+		var vp9 codecs.VP9Packet
+		_, err := vp9.Unmarshal(packet.Payload)
+		if err != nil || len(vp9.Payload) < 1 {
+			return false, false
+		}
+		if !vp9.B {
+			return false, true
+		}
+
+		if (vp9.Payload[0] & 0xc0) != 0x80 {
+			return false, false
+		}
+
+		profile := (vp9.Payload[0] >> 4) & 0x3
+		if profile != 3 {
+			return (vp9.Payload[0] & 0xC) == 0, true
+		}
+		return (vp9.Payload[0] & 0x6) == 0, true
+	} else if strings.EqualFold(codec, "video/av1") {
+		if len(packet.Payload) < 2 {
+			return false, true
+		}
+		// Z=0, N=1
+		if (packet.Payload[0] & 0x88) != 0x08 {
+			return false, true
+		}
+		w := (packet.Payload[0] & 0x30) >> 4
+
+		getObu := func(data []byte, last bool) ([]byte, int, bool) {
+			if last {
+				return data, len(data), false
+			}
+			offset := 0
+			length := 0
+			for {
+				if len(data) <= offset {
+					return nil, offset, offset > 0
+				}
+				l := data[offset]
+				length |= int(l&0x7f) << (offset * 7)
+				offset++
+				if (l & 0x80) == 0 {
+					break
+				}
+			}
+			if len(data) < offset+length {
+				return data[offset:], len(data), true
+			}
+			return data[offset : offset+length],
+				offset + length, false
+		}
+		offset := 1
+		i := 0
+		for {
+			obu, length, truncated :=
+				getObu(packet.Payload[offset:], int(w) == i+1)
+			if len(obu) < 1 {
+				return false, false
+			}
+			tpe := (obu[0] & 0x38) >> 3
+			switch i {
+			case 0:
+				// OBU_SEQUENCE_HEADER
+				if tpe != 1 {
+					return false, true
+				}
+			default:
+				// OBU_FRAME_HEADER or OBU_FRAME
+				if tpe == 3 || tpe == 6 {
+					if len(obu) < 2 {
+						return false, false
+					}
+					// show_existing_frame == 0
+					if (obu[1] & 0x80) != 0 {
+						return false, true
+					}
+					// frame_type == KEY_FRAME
+					return (obu[1] & 0x60) == 0, true
+				}
+			}
+			if truncated || i >= int(w) {
+				// the first frame header is in a second
+				// packet, give up.
+				return false, false
+			}
+			offset += length
+			i++
+		}
+	} else if strings.EqualFold(codec, "video/h264") {
+		if len(packet.Payload) < 1 {
+			return false, false
+		}
+		nalu := packet.Payload[0] & 0x1F
+		if nalu == 0 {
+			// reserved
+			return false, false
+		} else if nalu <= 23 {
+			// simple NALU
+			return nalu == 7, true
+		} else if nalu == 24 || nalu == 25 || nalu == 26 || nalu == 27 {
+			// STAP-A, STAP-B, MTAP16 or MTAP24
+			i := 1
+			if nalu == 25 || nalu == 26 || nalu == 27 {
+				// skip DON
+				i += 2
+			}
+			for i < len(packet.Payload) {
+				if i+2 > len(packet.Payload) {
+					return false, false
+				}
+				length := uint16(packet.Payload[i])<<8 |
+					uint16(packet.Payload[i+1])
+				i += 2
+				if i+int(length) > len(packet.Payload) {
+					return false, false
+				}
+				offset := 0
+				if nalu == 26 {
+					offset = 3
+				} else if nalu == 27 {
+					offset = 4
+				}
+				if offset >= int(length) {
+					return false, false
+				}
+				n := packet.Payload[i+offset] & 0x1F
+				if n == 7 {
+					// All of our keyframes are found in this codepath (specfically nalu==24, STAP-A)
+					// which seems to stem from using `rtph264pay` with `aggregate-mode=zero-latency` (will browsers do similar?)
+					return true, true
+				} else if n >= 24 {
+					// is this legal?
+					return false, false
+				}
+				i += int(length)
+			}
+			if i == len(packet.Payload) {
+				return false, true
+			}
+			return false, false
+		} else if nalu == 28 || nalu == 29 {
+			// FU-A or FU-B
+			if len(packet.Payload) < 2 {
+				return false, false
+			}
+			if (packet.Payload[1] & 0x80) == 0 {
+				// not a starting fragment
+				return false, true
+			}
+			return (packet.Payload[1]&0x1F == 7), true
+		}
+		return false, false
+	}
+	return false, false
+}
+
+func KeyframeDimensions(codec string, packet *rtp.Packet) (uint32, uint32) {
+	if strings.EqualFold(codec, "video/vp8") {
+		var vp8 codecs.VP8Packet
+		_, err := vp8.Unmarshal(packet.Payload)
+		if err != nil {
+			return 0, 0
+		}
+		if len(vp8.Payload) < 10 {
+			return 0, 0
+		}
+		raw := uint32(vp8.Payload[6]) | uint32(vp8.Payload[7])<<8 |
+			uint32(vp8.Payload[8])<<16 | uint32(vp8.Payload[9])<<24
+		width := raw & 0x3FFF
+		height := (raw >> 16) & 0x3FFF
+		return width, height
+	} else if strings.EqualFold(codec, "video/vp9") {
+		if packet == nil {
+			return 0, 0
+		}
+		var vp9 codecs.VP9Packet
+		_, err := vp9.Unmarshal(packet.Payload)
+		if err != nil {
+			return 0, 0
+		}
+		if !vp9.V {
+			return 0, 0
+		}
+		w := uint32(0)
+		h := uint32(0)
+		for i := range vp9.Width {
+			if i >= len(vp9.Height) {
+				break
+			}
+			if w < uint32(vp9.Width[i]) {
+				w = uint32(vp9.Width[i])
+			}
+			if h < uint32(vp9.Height[i]) {
+				h = uint32(vp9.Height[i])
+			}
+		}
+		return w, h
+	} else if strings.EqualFold(codec, "video/h264") {
+		fmt.Println("FIXME: parse SPS or find some lib that does?")
+		return 0, 0
+	} else {
+		return 0, 0
+	}
+}
+
 // Parse VP8 video and Write to WebM
 func pushVP8(rtpPacket *rtp.Packet) {
 	videoBuilder.Push(rtpPacket)
@@ -152,6 +378,38 @@ func pushVP8(rtpPacket *rtp.Packet) {
 			if videoWriter == nil || audioWriter == nil {
 				// Initialize WebM saver using received frame size.
 				startFFmpeg(width, height)
+			}
+		}
+		if videoWriter != nil {
+			videoTimestamp += sample.Samples
+			t := videoTimestamp / 90
+			if _, err := videoWriter.Write(videoKeyframe, int64(t), sample.Data); err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+// Parse H264 video and Write to WebM
+func pushH264(rtpPacket *rtp.Packet) {
+	videoBuilder.Push(rtpPacket)
+
+	for {
+		sample := videoBuilder.Pop()
+		if sample == nil {
+			return
+		}
+		// Read H264 header.
+		videoKeyframe, videoKeyframeKnown := Keyframe("video/h264", rtpPacket)
+		if videoKeyframe && videoKeyframeKnown{
+			// Keyframe has frame information.
+			/* FIXME: actually get these from bitstream */
+			width, height := KeyframeDimensions("video/h264", rtpPacket)
+
+			fmt.Println("Got H.264 key frame", width, "x", height)
+			if videoWriter == nil || audioWriter == nil {
+				// Initialize WebM saver using received frame size.
+				startFFmpeg(int(width), int(height))
 			}
 		}
 		if videoWriter != nil {
@@ -198,7 +456,7 @@ func main() {
 	// Watch the second stream
 	msg, err := handle.Message(map[string]interface{}{
 		"request": "watch",
-		"id":      1,
+		"id":      10,
 	}, nil)
 	if err != nil {
 		panic(err)
@@ -216,8 +474,7 @@ func main() {
 		}
 
 		audioBuilder = samplebuilder.New(10, &codecs.OpusPacket{})
-		videoBuilder = samplebuilder.New(10, &codecs.VP8Packet{})
-
+		videoBuilder = samplebuilder.New(10, &codecs.H264Packet{})
 
 		// Create a new RTCPeerConnection
 		var peerConnection *webrtc.PeerConnection
@@ -271,7 +528,7 @@ func main() {
 				case webrtc.RTPCodecTypeAudio:
 					pushOpus(rtp)
 				case webrtc.RTPCodecTypeVideo:
-					pushVP8(rtp)
+					pushH264(rtp)
 				}
 			}
 		})
